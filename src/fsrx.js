b82de09a11b8262532568235c03f1af0dfb03e31
Oscar Ponce <wigahluk@gmail.com>
2016-10-16 21:11:10 -0700

    Delete recursive implemented using RxJS.

--- src/fsrx.js
+++ src/fsrx.js
@@ -2,10 +2,40 @@ const fs = require('fs');
 const rx = require('rxjs');
 
 const readFile = rx.Observable.bindNodeCallback(fs.readFile);
-const exists = path => rx.Observable.bindNodeCallback(fs.access)(path);
+const readDir = rx.Observable.bindNodeCallback(fs.readdir);
+const unlink = rx.Observable.bindNodeCallback(fs.unlink);
+const rmdir = rx.Observable.bindNodeCallback(fs.rmdir);
+
+const lstat = path => rx.Observable.bindNodeCallback(fs.lstat)(path).map(s => { return { path: path, stats: s }; } );
+
+const exists = path => rx.Observable.bindNodeCallback(fs.access)(path).map(x => path);
+
+const traverse = path => readDir(path)
+    .flatMap(ar => rx.Observable.from(ar))
+    .map(p => `${path}/${p}`)
+    .flatMap(p => lstat(p))
+    .flatMap(s => {
+        if (s.stats.isDirectory()) {
+            return traverse(s.path);
+        }
+        return rx.Observable.of(s.path);
+    });
+
+const deleteRecursive = path => readDir(path)
+    .flatMap(ar => rx.Observable.from(ar))
+    .map(p => `${path}/${p}`)
+    .flatMap(p => lstat(p))
+    .flatMap(s => {
+        if (s.stats.isDirectory()) {
+            return deleteRecursive(s.path).count().flatMap(n => rmdir(s.path));
+        }
+        return unlink(s.path);
+    });
 
 module.exports = {
     readFile: readFile,
-    exists: exists
-
+    readDir: readDir,
+    lstat: lstat,
+    exists: exists,
+    rmrf: deleteRecursive
 };
--- src/okapi.js
+++ src/okapi.js
@@ -1,33 +1,12 @@
 const fs = require('./fsrx');
-const rx = require('rxjs');
-
-function clean (srcPath) {
-    console.log(`Cleaning ${srcPath}\n`);
-    deleteFolderRecursive(srcPath);
-    function deleteFolderRecursive (path) {
-        if( fs.existsSync(path) ) {
-            fs.readdirSync(path).forEach(function(file){
-                var curPath = `${path}/${file}`;
-                if(fs.lstatSync(curPath).isDirectory()) {
-                    deleteFolderRecursive(curPath);
-                } else {
-                    fs.unlinkSync(curPath);
-                }
-            });
-            fs.rmdirSync(path);
-        }
-    }
-}
 
 const clean = path => {
     fs.exists(path)
+        .flatMap(p => fs.rmrf(p))
         .subscribe(
-            p => clean(p),
+            p => { console.log('Deletion completed'); },
             e => { if (e.code !== 'ENOENT') { throw e; } }
         );
-    // fs.traverse(path).subscribe(
-    //     n => console.log(n)
-    // );
 };
 
 module.exports = {
