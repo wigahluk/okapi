f60b13dbcf5e6b22e70258d84898d265eef5958f
Oscar Ponce <wigahluk@gmail.com>
2016-10-18 21:18:31 -0700

    Implement build.

--- src/fsrx.js
+++ src/fsrx.js
@@ -5,6 +5,7 @@ const readFile = rx.Observable.bindNodeCallback(fs.readFile);
 const readDir = rx.Observable.bindNodeCallback(fs.readdir);
 const unlink = rx.Observable.bindNodeCallback(fs.unlink);
 const rmdir = rx.Observable.bindNodeCallback(fs.rmdir);
+const mkdir = rx.Observable.bindNodeCallback(fs.mkdir);
 
 const lstat = path => rx.Observable.bindNodeCallback(fs.lstat)(path).map(s => { return { path: path, stats: s }; } );
 
@@ -32,10 +33,28 @@ const deleteRecursive = path => readDir(path)
         return unlink(s.path);
     });
 
+const mkdirR = path => rx.Observable
+    .of(path)
+    .flatMap( p => {
+        if (!p) { throw new Error('Cannot create directory with empty name'); }
+        const a = p.split('/').filter(s => s.length > 0);
+        if (a.length === 0) { throw new Error('Cannot create directory with empty name'); }
+        return rx.Observable.from(a);
+    })
+    .scan((acc, v) => `${acc}/${v}`)
+    .flatMap(p =>
+        exists(p)
+            .map(p => { return {path: p, exists: true} })
+            .catch(e => rx.Observable.of({ path: p, exists: false })))
+    .flatMap(b => b.exists ? rx.Observable.of(b.path) : mkdir(b.path).map(x => b.path));
+
 module.exports = {
     readFile: readFile,
     readDir: readDir,
     lstat: lstat,
     exists: exists,
-    rmrf: deleteRecursive
+    rmrf: deleteRecursive,
+    traverse: traverse,
+    createWriteStream: fs.createWriteStream,
+    mkdirR: mkdirR
 };
--- src/okapi.js
+++ src/okapi.js
@@ -1,14 +1,52 @@
-const fs = require('./fsrx');
+const fsrx = require('./fsrx');
+const JSZip = require("jszip");
 
 const clean = path => {
-    fs.exists(path)
-        .flatMap(p => fs.rmrf(p))
+    fsrx.exists(path)
+        .flatMap(p => fsrx.rmrf(p))
         .subscribe(
-            p => { console.log('Deletion completed'); },
+            undefined,
+            e => { if (e.code !== 'ENOENT') { throw e; } },
+            () => { console.log('Deletion completed'); }
+        );
+};
+
+const enforceBuildPath = path => {
+    const idx = path.lastIndexOf('/');
+    if (idx < 0) { return rx.Observable.of(path)}
+    return fsrx.mkdirR(path.substr(0,idx));
+};
+
+const build = (source, dest, type) => {
+    const zip = new JSZip();
+
+    fsrx.exists(source)
+        .flatMap(p => fsrx.traverse(p))
+        .map(p => {
+            return { source: p, dest: type + p.substr(source.length) }
+        })
+        .flatMap(p =>
+            fsrx.readFile(p.source).map(bits => { return { dest: p.dest, content: bits }; })
+        )
+        .reduce((acc, p) => { zip.file(p.dest, p.content); return zip; }, zip)
+        .flatMap(zip => enforceBuildPath(dest).count().map(x => zip))
+        .subscribe(
+            zip => {
+                zip
+                    .generateNodeStream({
+                        streamFiles: false,
+                        compression: 'DEFLATE'
+                    })
+                    .pipe(fsrx.createWriteStream(`${dest}`))
+                    .on('finish', function () {
+                        console.log('Bundle completed.\n');
+                    });
+            },
             e => { if (e.code !== 'ENOENT') { throw e; } }
         );
 };
 
 module.exports = {
-    clean: clean
+    clean: clean,
+    build: build
 };
